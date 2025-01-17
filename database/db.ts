6044d1cbf407ac202ff4d5cf803ed98e052a953a
Tokiya Akiyoshi <tokiya_akiyoshi@m3sol.co.jp>
2025-01-17 23:50:37 +0900

    修正

--- database/db.ts
+++ database/db.ts
@@ -2,7 +2,6 @@
 import * as SQLite from 'expo-sqlite'
 
 export const openDatabaseAsync = async (dbName = 'local_database.db') =>{
-  console.log('Opening database') 
   return new Promise((resolve, reject) => {
     try {
       const db = SQLite.openDatabaseAsync(dbName)
--- hooks/useDatabase.ts
+++ hooks/useDatabase.ts
@@ -9,43 +9,17 @@ import { useEffect } from "react"
 export const useDatabase = () => {
   console.log('useDatabase hook called')
   useEffect(() => {
-    // // for test
-    // const listTables = async () => {
-    //   try {
-    //     const db = await openDatabaseAsync() as any
-    //     const tables = await db.getAllAsync(
-    //       `SELECT name FROM sqlite_master WHERE type='table';`
-    //     )
-    //     console.log('Tables:', tables)
-    //   } catch (error) {
-    //     console.error(error)
-    //   }
-    // }
-    // // for test
-    // const deleteDB = async () => {
-    //   try {
-    //     await deleteDatabaseAsync('local_database.db');
-    //     console.log('Database deleted successfully');
-    //   } catch (error) {
-    //     console.error(error);
-    //   }
-    // }
-
     const initDatabase = async () => {
-      console.log('Initializing database')
       try {
         const db = await openDatabaseAsync()
         await applyInitialSchema(db)
         // マイグレーション適用
         await applyMigrations(db)
-        console.log('Database initialized')
       } catch (error) {
         console.error(error)
         throw new Error('Failed to open database')
       }
     }
-    // deleteDB()
-    // listTables()
     initDatabase()
   }, []) // []を渡すことで初回レンダー時のみ実行される
 }
