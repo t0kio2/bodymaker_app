758f7d0fef6c4fa933247ac6809cd4927ea3f125
Tokiya Akiyoshi <tokiya_akiyoshi@m3sol.co.jp>
2025-01-28 22:19:09 +0900

    react-native-calendars追加

--- app/(tabs)/calendar.tsx
+++ app/(tabs)/calendar.tsx
@@ -1,17 +1,26 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, FlatList } from 'react-native'
 import React from 'react'
-import {Calendar as CalendarComponent, CalendarList, Agenda} from 'react-native-calendars'
+import {Calendar as CalendarComponent, LocaleConfig } from 'react-native-calendars'
+
+LocaleConfig.locales.jp = {
+  monthNames: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+  monthNamesShort: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+  dayNames: ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'],
+  dayNamesShort: ['日', '月', '火', '水', '木', '金', '土'],
+}
+LocaleConfig.defaultLocale = 'jp'
 
 const Calendar = () => {
   return (
-    <View>
-      <Text>calendar</Text>
+    <SafeAreaView className='h-full'>
       <CalendarComponent
         onDayPress={day => {
           console.log('selected day', day);
         }}
       />
-    </View>
+      
+      {/* <FlatList /> */}
+    </SafeAreaView>
   )
 }
 
